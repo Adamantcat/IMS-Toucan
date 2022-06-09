a84b54de5bf5b3535f6d4ecee3ba74b27b968ae2
ac138541 <julia.koch@ims.uni-stuttgart.de>
2022-06-09 15:47:28 +0200

    add class to get sentence embeddings from mbert

---
+++ Preprocessing/SentenceEmbeddingExtractor.py
@@ -0,0 +1,25 @@
+import torch
+from transformers import BertTokenizer, BertModel
+
+
+class SentenceEmbeddingExtractor:
+
+    def __init__(self, language="en", device=torch.device("cpu")):
+        #TODO: How to handle unsupported languages
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+        self.bert = BertModel.from_pretrained("bert-base-multilingual-cased")
+        self.device = device
+       
+        
+    def extract_sentence_embedding(self, text):
+        encoded_input = self.tokenizer(text, return_tensors='pt').to(self.device)
+        cls = self.bert(**encoded_input)['last_hidden_state'][0][0] # [batch][tokens]
+        #print(self.bert(**encoded_input)["pooler_output"].shape)
+        return cls
+
+
+if __name__ == '__main__':
+    text = "Hello World, this is a test."
+    ext = SentenceEmbeddingExtractor(language="en")
+    cls = ext.extract_sentence_embedding(text)
+    print(cls.shape)
